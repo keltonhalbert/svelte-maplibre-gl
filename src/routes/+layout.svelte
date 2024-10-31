5a97bead0e53c7379a40704f52d0641de2197828
Taku Fukada <naninunenor@gmail.com>
2024-10-31 13:24:58 +0900

    Geist font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import '../app.css';
+	import '$lib/assets/fonts/geist.css';
+
 	let { children } = $props();
 </script>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,17 @@
 import aspectRatio from '@tailwindcss/aspect-ratio';
 import typography from '@tailwindcss/typography';
+import defaultTheme from 'tailwindcss/defaultTheme';
 import type { Config } from 'tailwindcss';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Geist', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
 
 	plugins: [typography, aspectRatio]
