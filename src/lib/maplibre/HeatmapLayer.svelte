637de14dc36d07ba167674942293bde1c9436e36
Taku Fukada <naninunenor@gmail.com>
2024-11-02 16:45:52 +0900

    Add HeatmapLayer

---
+++ src/lib/maplibre/HeatmapLayer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { type Snippet } from 'svelte';
+	import VectorLayer from './_VectorLayer.svelte';
+	import type { HeatmapLayerSpecification } from 'maplibre-gl';
+
+	interface Props extends Omit<HeatmapLayerSpecification, 'source' | 'type' | 'source-layer'> {
+		sourceLayer?: HeatmapLayerSpecification['source-layer'];
+		beforeId?: string;
+		children?: Snippet;
+	}
+
+	let { children, ...props }: Props = $props();
+</script>
+
+<VectorLayer type="heatmap" {...props}>
+	{@render children?.()}
+</VectorLayer>
