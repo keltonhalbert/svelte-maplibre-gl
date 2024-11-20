37d2dffd45f7bcb0432a5f45e93a1f0b4a72a664
Taku Fukada <naninunenor@gmail.com>
2024-11-20 19:19:08 +0900

    Change syntax highlighter from PrismJS to Shiki

--- src/content/CodeBlock.svelte
+++ src/content/CodeBlock.svelte
@@ -1,18 +1,43 @@
 <script lang="ts">
-	import Prism from 'prismjs';
-	import 'prism-svelte';
+	import { createHighlighterCoreSync, createJavaScriptRegexEngine } from 'shiki';
+	import svelte from 'shiki/langs/svelte.mjs';
+	import dark from 'shiki/themes/github-dark-default.mjs';
 
 	let {
-		content,
-		lang = 'svelte'
+		content
 	}: {
 		content: string;
-		lang: string;
 	} = $props();
 
-	let highlighted = $derived(Prism.highlight(content, Prism.languages[lang], 'svelte'));
+	const shiki = createHighlighterCoreSync({
+		themes: [dark],
+		langs: [svelte],
+		engine: createJavaScriptRegexEngine()
+	});
+
+	const highlighted = $derived.by(() => {
+		return shiki.codeToHtml(content.trim(), { lang: 'svelte', theme: 'github-dark-default' });
+	});
 </script>
 
-<div class="my-6 text-[14px] subpixel-antialiased">
-	<pre class={`language-${lang}`}><code class={`language-${lang}`}>{@html highlighted}</code></pre>
+<div class="my-6 subpixel-antialiased">
+	{@html highlighted}
 </div>
+
+<style>
+	:global(.shiki code) {
+		tab-size: 2;
+		counter-reset: step;
+		counter-increment: step 0;
+	}
+
+	:global(.shiki code .line::before) {
+		content: counter(step);
+		counter-increment: step;
+		width: 1rem;
+		margin-right: 1.5rem;
+		display: inline-block;
+		text-align: right;
+		color: rgba(115, 138, 148, 0.4);
+	}
+</style>
