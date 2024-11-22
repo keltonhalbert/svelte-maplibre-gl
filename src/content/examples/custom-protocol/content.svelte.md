1d0e89d56b6bf7bf32a031eb68ea0c47add01826
Kanahiro <mediterranean1769@gmail.com>
2024-11-22 13:28:29 +0900

    feat: implement <Protocol/> for custom protocols

---
+++ src/content/examples/custom-protocol/content.svelte.md
@@ -0,0 +1,14 @@
+---
+title: Custom Protocols
+description: How to add custom protocols.
+---
+
+<script lang="ts">
+  import CustomProtocol from "./CustomProtocol.svelte";
+  import demoRaw from "./CustomProtocol.svelte?raw";
+  import CodeBlock from "../../CodeBlock.svelte";
+</script>
+
+<CustomProtocol />
+
+<CodeBlock content={demoRaw} />
---
+++ src/lib/maplibre/Protocol.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { onDestroy } from 'svelte';
+
+	import maplibregl from 'maplibre-gl';
+
+	const { name, loadFn }: { name: string; loadFn: maplibregl.AddProtocolAction } = $props();
+
+	$effect(() => {
+		maplibregl.addProtocol(name, loadFn); // no error even when already added
+	});
+
+	onDestroy(() => {
+		maplibregl.removeProtocol(name);
+	});
+</script>
