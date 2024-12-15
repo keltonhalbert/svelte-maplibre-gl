84023ec6ae5ece4ccf7fabed81a641d318b5cd4c
Taku Fukada <naninunenor@gmail.com>
2024-12-15 21:53:41 +0900

    temporary example toc

---
+++ src/routes/examples/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Index from '$lib/components/Index.svelte';
+
+	import { toc } from '$content/examples/toc.js';
+</script>
+
+<div id="toc" class="p-8">
+	<Index name="Examples" {toc} />
+</div>
+
+<style>
+	#toc {
+		view-transition-name: toc;
+	}
+</style>
