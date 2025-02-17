ff701c96e455aa50c453daaa58218d00628c9895
Taku Fukada <naninunenor@gmail.com>
2025-02-17 22:06:38 +0900

    feat: Support pmtiles.Protocol.add() (#93)

--- src/lib/maplibre/ext/pmtiles/PMTilesProtocol.svelte
+++ src/lib/maplibre/ext/pmtiles/PMTilesProtocol.svelte
@@ -1,9 +1,44 @@
 <script lang="ts">
 	import { Protocol as MapLibreProtocol } from 'svelte-maplibre-gl';
-	import { Protocol } from 'pmtiles';
+	import { Protocol, PMTiles } from 'pmtiles';
 
-	let { scheme = 'pmtiles' }: { scheme?: string } = $props();
-	const protocol = new Protocol();
+	let {
+		scheme = 'pmtiles',
+		metadata = false,
+		errorOnMissingTile = false,
+		pmtiles
+	}: {
+		/** URL scheme for the PMTilesProtocol */
+		scheme?: string;
+		/** Also load the metadata section of the PMTiles. required for some "inspect" functionality
+		 * and to automatically populate the map attribution. Requires an extra HTTP request.
+		 */
+		metadata?: boolean;
+		/** When a vector MVT tile is missing from the archive, raise an error instead of
+		 * returning the empty array. Not recommended. This is only to reproduce the behavior of ZXY tile APIs
+		 * which some applications depend on when overzooming. */
+		errorOnMissingTile?: boolean;
+		/** Array of PMTiles instances */
+		pmtiles?: PMTiles[];
+	} = $props();
+
+	const protocol = new Protocol({ metadata, errorOnMissingTile });
+
+	$effect(() => {
+		protocol.metadata = metadata;
+	});
+
+	$effect(() => {
+		protocol.errorOnMissingTile = errorOnMissingTile;
+	});
+
+	$effect(() => {
+		if (pmtiles) {
+			for (const pmtile of pmtiles) {
+				protocol.add(pmtile);
+			}
+		}
+	});
 </script>
 
 <MapLibreProtocol {scheme} loadFn={protocol.tile} />
